c412a2cd13539827eee9364da604be1c033e2ab0
Jannik Rößler <jannik.roessler@studmail.htw-aalen.de>
2024-07-04 00:16:29 +0200

    More work on Unity

--- unity/Lern-Bot/Assets/Scripts/RobotInteraction.cs
+++ unity/Lern-Bot/Assets/Scripts/RobotInteraction.cs
@@ -1,9 +1,15 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+
 using TMPro;
+
+using SFB;
+using WebSocketSharp;
 
 public class RobotInteraction : MonoBehaviour
 {
@@ -12,8 +18,13 @@ public class RobotInteraction : MonoBehaviour
     public TMP_Text speechBubbleText; // Der Text der Sprechblase
     public Button askQuestionButton; // Der Button zum Stellen einer Frage
     public Button uploadDocumentButton; // Der Button zum Hochladen eines Dokuments
+    public TMP_InputField questionInputField;
+
+    public string documentUploadUrl = "http://127.0.0.1:8000/documents";
+    public string websocketUrl = "ws://127.0.0.1:8000/ws/";
 
     private bool isUIVisible = false;
+    private WebSocket websocket;
 
     void Start()
     {
@@ -24,6 +35,19 @@ public class RobotInteraction : MonoBehaviour
         // Verstecke das UI-Panel und die Sprechblase zu Beginn
         uiPanel.SetActive(false);
         speechBubbleBackground.SetActive(false);
+
+        // WebSocket initialisieren
+        websocket = new WebSocket(websocketUrl);
+        websocket.OnMessage += (sender, e) =>
+        {
+            ShowWebSocketResponse(e.Data);
+        };
+        websocket.Connect();
+    }
+
+    void OnDestroy()
+    {
+        websocket.Close();
     }
 
     void OnMouseDown()
@@ -55,14 +79,57 @@ public class RobotInteraction : MonoBehaviour
     {
         // Logik zum Stellen einer Frage
         Debug.Log("Frage stellen");
-        // Hier kannst du Code hinzufügen, um eine Frage zu verarbeiten oder ein neues UI zu öffnen
+
+        string question = questionInputField.text;
+        websocket.Send(question);
     }
 
     void UploadDocument()
     {
         // Logik zum Hochladen eines Dokuments
         Debug.Log("Dokument hochladen");
-        // Hier kannst du Code hinzufügen, um den Datei-Upload-Prozess zu starten
+
+        StartCoroutine(UploadDocumentCoroutine());
+    }
+
+    IEnumerator UploadDocumentCoroutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Verwende StandaloneFileBrowser, um die Datei auszuwählen
+        string path = StandaloneFileBrowser.OpenFilePanel("Select Document", "", "", false)[0];
+        if (!string.IsNullOrEmpty(path))
+        {
+            byte[] fileData = File.ReadAllBytes(path);
+            StartCoroutine(UploadFile(path, fileData));
+        }
+    }
+
+    IEnumerator UploadFile(string path, byte[] fileData)
+    {
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("file", fileData, Path.GetFileName(path));
+
+        using (UnityWebRequest www = UnityWebRequest.Post(documentUploadUrl, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("File successfully uploaded");
+            }
+        }
+    }
+
+    void ShowWebSocketResponse(string response)
+    {
+        speechBubbleText.text = response;
+        speechBubbleBackground.SetActive(true);
+        //Invoke("HideSpeechBubble", 3f);
     }
 }
 
