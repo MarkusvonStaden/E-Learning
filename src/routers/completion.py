d4ce0ee671e87e04cdb84aabda41a94dd8d7f19e
Markus von Staden <markus@von-staden.net>
2024-07-08 11:57:55 +0200

    Add Chat History Capabilities

--- src/routers/completion.py
+++ src/routers/completion.py
@@ -13,6 +13,9 @@ Answer the question based only on the following context:
 
 ---
 
+Chat History:
+{chat_history}
+
 Answer the question based on the above context: {question}
 """
 
@@ -30,31 +33,34 @@ class ConnectionManager:
 embedding_function = OpenAIEmbeddings()
 vector_store = Chroma(embedding_function=embedding_function, persist_directory='chromadb')
 
-def ask(query):
+def ask(query, chat_history):
     results = vector_store.similarity_search_with_relevance_scores(query, 5)
     chat_model = ChatOpenAI(
         model = "gpt-4o"
     )
+    chat_history_text = "\n".join([f"User: {entry['user']}\nAssistant: {entry['assistant']}" for entry in chat_history])
     if len(results) == 0 or results[0][1] < 0.7:
-        response = chat_model.predict(query)
+        prompt_template = ChatPromptTemplate.from_template("Chat History:\n{chat_history}\n\nUser: {question}\nAssistant:")
+        prompt = prompt_template.format(chat_history=chat_history_text, question=query)
+        response = chat_model.predict(prompt)
     else:
         context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-        print(context_text)
         prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-        prompt = prompt_template.format(context=context_text, question=query)
-        response =  chat_model.predict(prompt)
+        prompt = prompt_template.format(context=context_text, chat_history=chat_history_text, question=query)
+        response = chat_model.predict(prompt)
     return response
-
 
 manager = ConnectionManager()
 
 @router.websocket("/ws/")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
+    history = []
     try:
         while True:
             data = await websocket.receive_text()
-            response = ask(data)
+            response = ask(data, history)
             await websocket.send_text(response)
+            history.append({"user": data, "assistant": response})
     except WebSocketDisconnect:
         manager.disconnect(websocket)
