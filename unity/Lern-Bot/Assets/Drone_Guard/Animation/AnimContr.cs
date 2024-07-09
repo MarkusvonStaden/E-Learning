687ab3e5434e3b9fd4424e547f36bea8134173cb
Jannik Rößler <jannik.roessler@studmail.htw-aalen.de>
2024-07-09 20:13:51 +0200

    More Unity work - first Game Version

--- unity/Lern-Bot/Assets/Drone_Guard/Animation/AnimContr.cs
+++ unity/Lern-Bot/Assets/Drone_Guard/Animation/AnimContr.cs
@@ -6,31 +6,12 @@ public class AnimContr : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator anim;
+
     void Start()
     {
         anim = GetComponent<Animator>();
 
+        anim.speed = 2;
         anim.Play("WakeUp");       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        /*if (Input.GetKeyDown("s"))
-        {
-            anim.Play("ShutDown");
-        }
-
-        if (Input.GetKeyDown("w"))
-        {
-            anim.Play("WakeUp");
-        }
-
-        if (Input.GetKeyDown("d"))
-        {
-            anim.Play("Destroyed");
-        }*/
-
     }
 }
--- unity/Lern-Bot/Assets/Scripts/RobotInteraction.cs
+++ unity/Lern-Bot/Assets/Scripts/RobotInteraction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,11 +14,14 @@ using WebSocketSharp;
 
 public class RobotInteraction : MonoBehaviour
 {
-    public GameObject uiPanel; // Das Interface Panel
-    public GameObject speechBubbleBackground; // Der Hintergrund der Sprechblase
-    public TMP_Text speechBubbleText; // Der Text der Sprechblase
-    public Button askQuestionButton; // Der Button zum Stellen einer Frage
-    public Button uploadDocumentButton; // Der Button zum Hochladen eines Dokuments
+    public GameObject uiPanel;
+    public GameObject uiPanel2;
+    public GameObject uiPanel3;
+    public GameObject speechBubbleBackground;
+    public TMP_Text speechBubbleText;
+    public Button askQuestionButton;
+    public Button uploadDocumentButton;
+    public Button quitButton;
     public TMP_InputField questionInputField;
 
     public string documentUploadUrl = "http://127.0.0.1:8000/documents";
@@ -26,22 +30,45 @@ public class RobotInteraction : MonoBehaviour
     private bool isUIVisible = false;
     private WebSocket websocket;
 
+    private string receivedResponse;
+    private bool responseReceived = false;
+    private bool waitingForResponse = false;
+
     void Start()
     {
-        // Setze die OnClick-Listener für die Buttons
+        // On-click listener for buttons
         askQuestionButton.onClick.AddListener(AskQuestion);
         uploadDocumentButton.onClick.AddListener(UploadDocument);
+        quitButton.onClick.AddListener(QuitGame);
 
-        // Verstecke das UI-Panel und die Sprechblase zu Beginn
+        // Hide UI at beginning
         uiPanel.SetActive(false);
+        uiPanel2.SetActive(false);
+        uiPanel3.SetActive(false);
         speechBubbleBackground.SetActive(false);
 
-        // WebSocket initialisieren
+        // Activate UIPanel2 after 6 secs
+        StartCoroutine(ActivatePanelAfterDelay(6f));
+
+        // Initialize WebSocket
         websocket = new WebSocket(websocketUrl);
-        websocket.OnMessage += (sender, e) =>
+        websocket.OnOpen += (sender, e) =>
         {
-            ShowWebSocketResponse(e.Data);
+            Debug.Log("WebSocket-Connection opened");
         };
+
+        websocket.OnMessage += WebSocket_OnMessage;
+
+        websocket.OnError += (sender, e) =>
+        {
+            Debug.LogError("WebSocket-Error: " + e.Message);
+        };
+
+        websocket.OnClose += (sender, e) =>
+        {
+            Debug.Log("WebSocket-Connection closed");
+        };
+
         websocket.Connect();
     }
 
@@ -60,34 +87,92 @@ public class RobotInteraction : MonoBehaviour
     {
         isUIVisible = !isUIVisible;
         uiPanel.SetActive(isUIVisible);
+        uiPanel2.SetActive(!isUIVisible);
     }
 
     void ShowSpeechBubble()
     {
-        speechBubbleText.text = "Guten Tag, wie kann ich dir heute helfen?";
+        speechBubbleText.text = "Wie kann ich dir heute helfen?";
         speechBubbleBackground.SetActive(true);
-        // Deaktiviere die Sprechblase nach 3 Sekunden
-        //Invoke("HideSpeechBubble", 3f);
     }
 
-    void HideSpeechBubble()
+    IEnumerator ActivatePanelAfterDelay(float delay)
     {
-        speechBubbleBackground.SetActive(false);
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        uiPanel2.SetActive(true); // Activate the UI panels
+        uiPanel3.SetActive(true);
     }
 
     void AskQuestion()
-    {
-        // Logik zum Stellen einer Frage
-        Debug.Log("Frage stellen");
+    {   
+        if(waitingForResponse)
+        {
+            Debug.Log("Waiting for previous response.");
+            return;
+        }
 
         string question = questionInputField.text;
-        websocket.Send(question);
+        Debug.Log("Ask question: " + question);
+        
+        // Perform WebSocket operations
+        if(websocket != null && websocket.ReadyState == WebSocketState.Open && question != "")
+        {
+            websocket.Send(question);
+            Debug.Log("Question sent");
+
+            waitingForResponse = true;
+
+            // Wait for response
+            StartCoroutine(WaitForResponseCoroutine());
+        }
+        else if(question == "")
+        {
+            Debug.Log("Enter a question first");
+
+            speechBubbleText.text = "Bitte gib unter 'Enter Text...' erste eine Frage ein und drücke dann auf 'Eine Frage stellen'";
+            speechBubbleText.fontSize = 36;
+        }
+        else
+        {
+            Debug.LogError("WebSocket-Connection not opened");
+        }
+    }
+
+    IEnumerator WaitForResponseCoroutine()
+    {
+        // Wait until response is received
+        while(!responseReceived)
+        {
+            yield return null;
+        }
+
+        // Process received response
+        UpdateSpeechBubble();
+    }
+
+    void WebSocket_OnMessage(object sender, MessageEventArgs e)
+    {
+        // Handle WebSocket message received
+        Debug.Log("Message received");
+        receivedResponse = e.Data;
+        responseReceived = true;
+    }
+
+    void UpdateSpeechBubble()
+    {
+        // Display received response in speech bubble
+        speechBubbleText.text = receivedResponse;
+        speechBubbleText.fontSize = 36;
+        speechBubbleBackground.SetActive(true);
+
+        Debug.Log("Answer updated");
+        waitingForResponse = false;
+        responseReceived = false;
     }
 
     void UploadDocument()
     {
-        // Logik zum Hochladen eines Dokuments
-        Debug.Log("Dokument hochladen");
+        Debug.Log("Upload document");
 
         StartCoroutine(UploadDocumentCoroutine());
     }
@@ -96,26 +181,38 @@ public class RobotInteraction : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        // Verwende StandaloneFileBrowser, um die Datei auszuwählen
-        string path = StandaloneFileBrowser.OpenFilePanel("Select Document", "", "", false)[0];
-        if (!string.IsNullOrEmpty(path))
+        var paths = StandaloneFileBrowser.OpenFilePanel("Select Document", "", "pdf", false);
+        if(paths.Length > 0)
         {
-            byte[] fileData = File.ReadAllBytes(path);
-            StartCoroutine(UploadFile(path, fileData));
+            string path = paths[0];
+            if(!string.IsNullOrEmpty(path))
+            {
+                if(Path.GetExtension(path).ToLower() == ".pdf")
+                {
+                    byte[] fileData = File.ReadAllBytes(path);
+                    StartCoroutine(UploadFile(path, fileData));
+                }
+                else
+                {
+                    Debug.LogError("Only PDF files are allowed.");
+                }
+            }
         }
     }
 
     IEnumerator UploadFile(string path, byte[] fileData)
     {
         WWWForm form = new WWWForm();
-        form.AddBinaryData("file", fileData, Path.GetFileName(path));
+        form.AddBinaryData("file", fileData, Path.GetFileName(path), "application/pdf");
 
-        using (UnityWebRequest www = UnityWebRequest.Post(documentUploadUrl, form))
+        using(UnityWebRequest www = UnityWebRequest.Post(documentUploadUrl, form))
         {
+            Debug.Log("Uploading file: " + Path.GetFileName(path));
             yield return www.SendWebRequest();
 
-            if (www.result != UnityWebRequest.Result.Success)
+            if(www.result != UnityWebRequest.Result.Success)
             {
+                Debug.Log(www.downloadHandler.text);
                 Debug.Log(www.error);
             }
             else
@@ -125,11 +222,14 @@ public class RobotInteraction : MonoBehaviour
         }
     }
 
-    void ShowWebSocketResponse(string response)
+    void QuitGame()
     {
-        speechBubbleText.text = response;
-        speechBubbleBackground.SetActive(true);
-        //Invoke("HideSpeechBubble", 3f);
+        // Quit the game
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
 
